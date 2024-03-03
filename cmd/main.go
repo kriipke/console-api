fe733b9f4e5b0c29badfd264be8047e3c6a9f2fc
Ubuntu <spencer>
2024-03-03 01:17:30 +0000

    FEATURE added logging of HTTP headers as gin handlers

--- cmd/main.go
+++ cmd/main.go
@@ -2,15 +2,22 @@ package main
 
 import (
 	"log"
-	"fmt"
 	"time"
+	"strings"
 	"net/http"	
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/kriipke/console-api/pkg/controllers"
 	"github.com/kriipke/console-api/pkg/initializers"
 	"github.com/kriipke/console-api/pkg/routes"
+	"github.com/charmbracelet/lipgloss"
+	"github.com/fatih/color"
+	//"github.com/charmbracelet/lipgloss/table"
+
 )
+
+var style_keys = lipgloss.NewStyle().Bold(true).Foreground(lipgloss.Color("5"))
+var style_values = lipgloss.NewStyle().Foreground(lipgloss.Color("8"))
 
 var (
 	server              *gin.Engine
@@ -20,6 +27,12 @@ var (
 	UserController      controllers.UserController
 	UserRouteController routes.UserRouteController
 )
+// function to convert an array to string
+func arrayToString(arr []string) string {
+
+   // seperating string elements with -
+   return strings.Join([]string(arr), ", ")
+}
 
 func init() {
 	config, err := initializers.LoadConfig("configs")
@@ -35,6 +48,7 @@ func init() {
 	UserController = controllers.NewUserController(initializers.DB)
 	UserRouteController = routes.NewRouteUserController(UserController)
 
+	gin.ForceConsoleColor()
 	server = gin.Default()
 }
 
@@ -43,6 +57,9 @@ func main() {
 	if err != nil {
 		log.Fatal("? Could not load environment variables", err)
 	}
+
+	//consoleApiHost := os.Getenv("CONSOLE_API_HOST")
+	//consoleApiPort := os.Getenv("CONSOLE_API_PORT")
 
 	corsConfig := cors.DefaultConfig()
 	corsConfig.AllowOrigins = []string{"http://0.0.0.0:8080", config.ClientOrigin}
@@ -53,8 +70,8 @@ func main() {
 
 	//server.Use(cors.New(corsConfig))
 	server.Use(corsMiddleware())
-	server.Use(RequestLogger())
 	server.Use(ResponseLogger())
+	server.Use(RequestLogger())
 
 	router := server.Group("/api")
 	router.GET("/healthchecker", func(ctx *gin.Context) {
@@ -73,16 +90,27 @@ func RequestLogger() gin.HandlerFunc {
     return func(c *gin.Context) {
         t := time.Now()
 
+				 c1 := color.New(color.FgMagenta, color.Bold)
+				 c2 := color.New(color.FgWhite)
+				 c3 := color.New(color.FgYellow)
+				 c4 := color.New(color.FgCyan, color.Bold)
+				 c5 := color.New(color.FgWhite, color.Bold)
+
         c.Next()
 
-        latency := time.Since(t)
+				latency := time.Since(t)
 
-        fmt.Printf("%s %s %s %s\n",
-            c.Request.Method,
-            c.Request.RequestURI,
-            c.Request.Proto,
-            latency,
-        )
+        c4.Printf("\n  %s", c.Request.Method)
+				c5.Printf("%s  ", c.Request.RequestURI)
+				c1.Printf("%s  ", c.Request.Proto)
+				c2.Printf("%s\n",  latency)
+
+				for name, values := range c.Request.Header {
+						for _, value := range values {
+							c3.Printf("   %-20s:", name)
+							c2.Printf("%-40s\n", value)
+						}
+				}
     }
 }
 
@@ -92,13 +120,26 @@ func ResponseLogger() gin.HandlerFunc {
 
         c.Next()
 
-        fmt.Printf("%d %s %s\n",
-            c.Writer.Status(),
-            c.Request.Method,
-            c.Request.RequestURI,
-        )
+				//c1 := color.New(color.FgMagenta, color.Bold)
+				c2 := color.New(color.FgWhite)
+				c3 := color.New(color.FgYellow)
+			  c4 := color.New(color.FgCyan, color.Bold)
+			  c5 := color.New(color.FgWhite, color.Bold)
+
+        c4.Printf("\n   %s", c.Writer.Status())
+				c4.Printf("%s  ", c.Request.Method)
+				c5.Printf("%s\n",  c.Request.RequestURI)
+
+				for name, values := range c.Writer.Header() {
+						for _, value := range values {
+							c3.Printf("    %-40s:", name)
+							c2.Printf("%-80s\n", value)
+						}
+				}
+
     }
 }
+
 
 func corsMiddleware() gin.HandlerFunc {
     return func(c *gin.Context) {
--- go.mod
+++ go.mod
@@ -14,9 +14,12 @@ require (
 )
 
 require (
+	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
 	github.com/bytedance/sonic v1.10.1 // indirect
+	github.com/charmbracelet/lipgloss v0.9.1 // indirect
 	github.com/chenzhuoyu/base64x v0.0.0-20230717121745-296ad89f973d // indirect
 	github.com/chenzhuoyu/iasm v0.9.0 // indirect
+	github.com/fatih/color v1.16.0 // indirect
 	github.com/fsnotify/fsnotify v1.7.0 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.2 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
@@ -34,12 +37,18 @@ require (
 	github.com/k3a/html2text v1.2.1 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.5 // indirect
 	github.com/leodido/go-urn v1.2.4 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
-	github.com/mattn/go-isatty v0.0.19 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/muesli/reflow v0.3.0 // indirect
+	github.com/muesli/termenv v0.15.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.1.0 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/sagikazarmark/locafero v0.4.0 // indirect
 	github.com/sagikazarmark/slog-shim v0.1.0 // indirect
 	github.com/sourcegraph/conc v0.3.0 // indirect
