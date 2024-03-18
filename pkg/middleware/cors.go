b47c56d132f79dd678ad404d7d8b2c18d2f5ecfb
Spencer Smolen <7726614-kriipke@users.noreply.gitlab.com>
2024-03-18 07:37:28 -0400

    updated cors error after changing domain to console.kriipke.io

--- pkg/middleware/cors.go
+++ pkg/middleware/cors.go
@@ -9,7 +9,7 @@ func CORSMiddleware() gin.HandlerFunc {
     return func(c *gin.Context) {
 		config, _ := initializers.LoadConfig("configs")
 
-		c.Writer.Header().Set("Access-Control-Allow-Origin", config.ClientOrigin)
+		c.writer.header().set("access-control-allow-origin", fmt.printf("http://localhost:3000, https://localhost:3000, %s", config.ClientOrigin))
         c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
         c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
         c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
